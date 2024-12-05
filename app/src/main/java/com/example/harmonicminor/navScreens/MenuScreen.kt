e9c55ed75aa96e7d4c22c4d9744a5c78c01d231a
vctr23 <vsanchezmel23@gmail.com>
2024-12-05 13:00:01 +0100

    Adding a country selection in shop menu

--- app/src/main/java/com/example/harmonicminor/navScreens/MenuScreen.kt
+++ app/src/main/java/com/example/harmonicminor/navScreens/MenuScreen.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,14 +18,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -38,8 +33,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -55,7 +48,6 @@ import com.example.harmonicminor.ui.theme.backgroundAccentColor
 import com.example.harmonicminor.ui.theme.backgroundColor
 import com.example.harmonicminor.ui.theme.borderColor
 import com.example.harmonicminor.ui.theme.iconColor
-import com.example.harmonicminor.ui.theme.lightAccentColor
 import com.example.harmonicminor.ui.theme.textColor
 
 
@@ -88,6 +80,8 @@ fun Menu(modifier: Modifier = Modifier) {
                 fontSize = 16.sp,
 
             )
+            CountrySelectionItem()
+            Spacer(modifier = Modifier.padding(vertical = 2.dp))
             LanguageSelectionItem()
         }
     }
@@ -100,7 +94,7 @@ fun Profile(){
                 .padding(horizontal = 16.dp)
                 .height(110.dp)
                 .fillMaxWidth()
-                .border(border = BorderStroke(1.dp, borderColor),
+                .border(border = BorderStroke(2.dp, borderColor),
                     shape = RoundedCornerShape(20.dp)
                 ),
             verticalAlignment = Alignment.CenterVertically,
@@ -145,7 +139,7 @@ fun LanguageSelectionItem(){
             .clickable {
                 showBottomSheet = true
             }
-            .border(BorderStroke(1.dp, borderColor),
+            .border(BorderStroke(2.dp, borderColor),
                 shape = RoundedCornerShape(10.dp)),
         verticalAlignment = Alignment.CenterVertically
     ){
@@ -201,6 +195,58 @@ fun LanguageSelectionItem(){
                             color = textColor,
                             modifier = Modifier.padding(start = 8.dp)
                         )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CountrySelectionItem(){
+    var showBottomSheet by rememberSaveable { mutableStateOf(false) }
+    val customIcon = ImageVector.vectorResource(R.drawable.language)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .clickable {
+                showBottomSheet = true
+            }
+            .border(BorderStroke(2.dp, borderColor),
+                shape = RoundedCornerShape(10.dp)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(modifier = Modifier.padding(horizontal = 8.dp, vertical = 16.dp))
+        Icon(customIcon, contentDescription = null, tint = iconColor)
+        Spacer(modifier = Modifier.padding(horizontal = 8.dp))
+        Text(
+            text = stringResource(R.string.country),
+            color = textColor,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = stringResource(R.string.app_country),
+            color = textColor
+        )
+        Icon(
+            Icons.AutoMirrored.Filled.KeyboardArrowRight,
+            contentDescription = null,
+            tint = iconColor
+        )
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = { showBottomSheet = false },
+                modifier = Modifier.fillMaxSize(),
+                containerColor = backgroundAccentColor
+            ) {
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = stringResource(R.string.available_languages), color = textColor)
+                    Row(){
+
                     }
                 }
             }
