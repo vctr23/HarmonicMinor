61bfd03ed62153dfcbdce9f533de9bd0ec928d83
vctr23 <vsanchezmel23@gmail.com>
2024-12-09 18:19:34 +0100

    Added a supressLint for API33

--- app/src/main/java/com/example/harmonicminor/screens/MainScreen.kt
+++ app/src/main/java/com/example/harmonicminor/screens/MainScreen.kt
@@ -1,7 +1,7 @@
 package com.example.harmonicminor.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -71,6 +71,7 @@ fun Main(navController: NavController, modifier: Modifier = Modifier) {
                     NavigationBarItem(
                         selected = selected == index,
                         onClick = {
+
                             selected = index
                         },
                         icon = {
@@ -96,6 +97,7 @@ fun Main(navController: NavController, modifier: Modifier = Modifier) {
     }
 }
 
+@SuppressLint("NewApi")
 @Composable
 fun ContentScreen(modifier: Modifier = Modifier, selected: Int) {
     when (selected) {
